3a50d9dccd026e0f9ae5147de369d6c5042fbc7c
Twahid Uddin <Twahiduddin@aol.com>
2022-04-11 05:17:09 -0400

    Time added directly in App.js this time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const getCurrentTime = () => {
+    const time = new Date();
+    return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+  }
+  
+  const [time, setTime] = useState(getCurrentTime());
+  
+  setInterval(() => {
+    setTime(getCurrentTime());
+  }, 1000);
+
   return (
     <div className="App">
-     <p>Hello User</p>
+      <div>
+        <p>Hello User</p>
+        <p>{time}</p>
+      </div>
     </div>
   );
 }
