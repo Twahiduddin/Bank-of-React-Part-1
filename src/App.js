734e70564e81e9ebe8df92f63972d1e39cbd4524
Twahid Uddin <Twahiduddin@aol.com>
2022-04-11 04:52:35 -0400

    Starting fresh, new approach

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 
 import './App.css';
-import Clock from './Clock';
-import Credit from './Credits';
-import Debit from './Debits';
 
 function App() {
   return (
     <div className="App">
      <p>Hello User</p>
-     <Clock></Clock>
-     <Credit></Credit>
-     <Debit></Debit>
     </div>
   );
 }
