6b13c527fb698438937620e6c35c2aa612e3437e
Twahid Uddin <Twahiduddin@aol.com>
2022-04-12 16:33:03 -0400

    styling for row added

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
 import { Fragment } from 'react';
+import styles from './Row.module.css'
 
 const Row = ({transaction}) => {
     return (
         <Fragment>
-          <div>
+          <div className={styles.row}> 
             <div>
               <h5>{transaction.description}</h5>
               <p>{transaction.date}</p>
